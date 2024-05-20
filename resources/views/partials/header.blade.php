dca4f6cebcdf5d9efb5993d26e31b27519ee08c8
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-05-20 18:05:08 +0200

    feat: header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container d-flex justify-content-between">
+            <a href="{{route('home')}}">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+            </a>
+            <div class="navbar" id="navbarNavDropdown">
+                <ul class="navbar-nav fw-bold">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">GAMES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SHOP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
